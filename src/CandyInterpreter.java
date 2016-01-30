90ac76deab9f754021c4866693ff1f4f5d4dc83b
Daniel Sauve <djsauve@rogers.com>
2016-01-29 20:01:21 -0500

    Added writing to file

--- src/CandyInterpreter.java
+++ src/CandyInterpreter.java
@@ -1,8 +1,19 @@
+import java.io.*;
 
 public class CandyInterpreter {
+	public static String OUTPUTFILE = "output.py";
+
 
 	public void writeToFile(String input) {
-		
+		File file = new File(OUTPUTFILE);
+		BufferedWriter writer = null;
+		try {
+			writer = new BufferedWriter(new FileWriter(file));
+			writer.write(input + "\n");
+			if (writer != null) writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void writeNewVariable(String varName) {
