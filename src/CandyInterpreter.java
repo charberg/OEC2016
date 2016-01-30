fd0ffe5a5ede6c3a09d3fb3d3f5eb64f4603b52f
CharBerg <charlesbergeron@cmail.carleton.ca>
2016-01-29 20:26:14 -0500

    Added many functions to python interpreter

--- src/CandyInterpreter.java
+++ src/CandyInterpreter.java
@@ -1,7 +1,11 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class CandyInterpreter {
 	public static String OUTPUTFILE = "output.py";
+	
+	private int tabIndex = 0;
 
 
 	public void writeToFile(String input) {
@@ -9,15 +13,45 @@ public class CandyInterpreter {
 		BufferedWriter writer = null;
 		try {
 			writer = new BufferedWriter(new FileWriter(file));
-			writer.write(input + "\n");
+			writer.write(constructTabs() + input + "\n");
 			if (writer != null) writer.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public void writeNewVariable(String varName) {
-		writeToFile(varName + " = ");
+	public String constructTabs() {
+		return String.join("", Collections.nCopies(tabIndex, "/t"));
 	}
+	
+	public void writeNewVariable(String varName, String value) {
+		writeToFile(varName + " = " + value);
+	}
+	
+	public void printVariable(String varName) {
+		writeToFile("print(" + varName + ")");
+	}
+	
+	public void writeEquals(String varName1, String varName2) {
+		writeToFile("if " + varName1 + " == " + varName2 + ":");
+		tabIndex++;
+	}
+	
+	public void endIf() {
+		tabIndex--;
+	}
+	
+	public void writeWhile(int loops) {
+		writeToFile("for _ in range(" + loops + ")");
+		tabIndex++;
+	}
+	
+	public void endWhile() {
+		tabIndex--;
+	}
+	
+
+	
+	
 	
 }
