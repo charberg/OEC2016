c0365b1d45790eec848c7433294cfd0fcc2e9dfb
Daniel Sauve <djsauve@rogers.com>
2016-01-29 20:22:54 -0500

    Added method to run Python code

--- src/CandyInterpreter.java
+++ src/CandyInterpreter.java
@@ -19,5 +19,24 @@ public class CandyInterpreter {
 	public void writeNewVariable(String varName) {
 		writeToFile(varName + " = ");
 	}
-	
+
+	public void runCode(){
+		ProcessBuilder pythonCode = new ProcessBuilder("python", OUTPUTFILE);
+		Process python = null;
+		try {
+			python = pythonCode.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if (python != null){
+			BufferedReader in = new BufferedReader(new InputStreamReader(python.getInputStream()));
+			String input = "";
+			try {
+				input = in.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			System.out.println("Value returned is: " + input);
+		}
+	}
 }
